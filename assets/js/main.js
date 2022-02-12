920ae7b5ce71c8cfbb13366eaffde123ec0b9ff2
azazmuzaffar <sp18-bcs-035@cuiwah.edu.pk>
2022-02-12 17:04:32 +0500

    Issue with the scroll slider...

--- assets/js/main.js
+++ assets/js/main.js
@@ -711,3 +711,21 @@ $('#max-bedroom').on('input', function () {
 })(jQuery);
 
 
+function SmoothVerticalScrolling(e, time, where) {
+  var eTop = e.getBoundingClientRect().top;
+  var eAmt = eTop / 100;
+  var curTime = 0;
+  while (curTime <= time) {
+      window.setTimeout(SVS_B, curTime, eAmt, where);
+      curTime += time / 100;
+  }
+}
+
+function SVS_B(eAmt, where) {
+  if(where == "center" || where == "")
+      window.scrollBy(0, eAmt / 2);
+  if (where == "top")
+      window.scrollBy(0, eAmt);
+}
+
+SmoothVerticalScrolling(myelement, 275, "center");
